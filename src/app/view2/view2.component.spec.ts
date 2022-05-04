cffdeefd38b7b5174aab504873884ce2b10f5e22
Jason Beres <jasonberes@gmail.com>
2022-05-04 15:42:17 -0400

    GridDemoDeleteMe initial version

---
+++ src/app/view2/view2.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { IgxGridModule } from 'igniteui-angular';
+import { View2Component } from './view2.component';
+
+describe('View2Component', () => {
+  let component: View2Component;
+  let fixture: ComponentFixture<View2Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ View2Component ],
+      imports: [ NoopAnimationsModule, FormsModule, IgxGridModule ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(View2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/view2/view2.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NorthwindService } from '../services/northwind.service';
+
+@Component({
+  selector: 'app-view2',
+  templateUrl: './view2.component.html',
+  styleUrls: ['./view2.component.scss']
+})
+export class View2Component implements OnInit {
+  public northwindCustomers: any = null;
+  public northwindEmployees: any = null;
+
+  constructor(
+    private northwindService: NorthwindService,
+  ) {}
+
+  ngOnInit() {
+    // depending on implementation, data subscriptions might need to be unsubbed later
+    this.northwindService.getData('Customers').subscribe(data => this.northwindCustomers = data);
+    this.northwindService.getData('Employees').subscribe(data => this.northwindEmployees = data);
+  }
+}
